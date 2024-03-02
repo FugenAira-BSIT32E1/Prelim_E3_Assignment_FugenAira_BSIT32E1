80f3eb9542853f159aa507c71cf2bf882378a2ff
AIRA FUGEN <airafugen14@gmail.com>
2024-03-02 11:16:18 +0800

    Add project files.

---
+++ Controllers/SchoolController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PRELIM_E3_Assignment_FugenAira_BSIT_32E1.Models;
+using System.Diagnostics;
+
+namespace PRELIM_E3_Assignment_FugenAira_BSIT_32E1.Controllers
+{
+    public class SchoolController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public SchoolController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/School.cs
@@ -0,0 +1,10 @@
+﻿namespace PRELIM_E3_Assignment_FugenAira_BSIT_32E1.Models
+{
+    public class School
+    {
+        public int Schedule { get; set; }
+        public String Day { get; set; }
+        public int Time { get; set; }
+
+    }
+}
